e70c1b49d45fcd39a0acdcd22c4143c80a57e1e1
code81-true <codegit81@gmail.com>
2026-05-01 16:51:08 +0100

    fix: deploy.sh references main branch

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/scout
 git clean -fd sessions/
-git pull origin master
+git pull origin main
 source venv/bin/activate
 pip install -r requirements.txt -q
 systemctl restart scout
